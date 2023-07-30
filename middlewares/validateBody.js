989e0d4e29b11cb64879d1578734924caaf08a70
SyvonenkoOleh <115041334+SyvonenkoOleh@users.noreply.github.com>
2023-07-30 23:43:48 +0300

    HW3.1

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -2,14 +2,20 @@ const { HttpError } = require("../helpers");
 
 const validateBody = (schema) => {
   const func = (req, res, next) => {
-    if (Object.keys(req.body).length === 0) {
-      next(HttpError(400, "missing fields"));
+    if (!req.body || Object.keys(req.body).length === 0) {
+      if (req.method === "PATCH") {
+        return next(HttpError(400, "missing field favorite"));
+      } else {
+        return next(HttpError(400, "missing fields"));
+      }
     }
+
     const { error } = schema.validate(req.body);
     if (error) {
       const fieldName = error.details[0].context.label;
-      next(HttpError(400, `missing required ${fieldName} field`));
+      return next(HttpError(400, `missing required ${fieldName} field`));
     }
+
     next();
   };
   return func;
